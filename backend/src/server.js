2b6187b9fceb25de4b2d17f032d5801aae8a3ab4
P.talent-ajieh <talented037l@gmail.com>
2026-04-22 12:56:19 +0100

    feat: add ORM

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,42 @@
 import express from 'express';
 import postsRoutes from './routes/posts_routes.js'
+import { config } from 'dotenv'
+import { connectDB, disconnectDB } from './config/db.js'
+
 
 const app = express()
-const PORT = 8080
+
+config()
+connectDB()
 
 app.use('/feed', postsRoutes)
 
-
+const PORT = 8080
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Rejection:", err)
+    server.close(async () => {
+        await disconnectDB()
+        process.exit(1)
+    })
+})
+
+process.on("uncaughtException", async (err) => {
+    console.error("Uncaught Exception:", err)
+    await disconnectDB()
+    process.exit(1)
+})
+
+process.on("SIGTERM", async () => {
+    console.log("SIGTERM received, shutting down gracefully...")
+    server.close(async () => {
+        await disconnectDB()
+        console.log("Server closed, exiting process.")
+        process.exit(0)
+    })
+})
+
+
